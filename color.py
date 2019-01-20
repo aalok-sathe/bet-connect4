283b3348356e4341dae6aff03912bd0b256e6d79
aalok.sathe <aalok.sathe@gmail.com>
2019-01-19 21:16:25 -0500

    add more posix colors

--- color.py
+++ color.py
@@ -3,10 +3,18 @@ This file houses POSIX codes to format text in a terminal.
 This file and its contents is to be treated as a Python source file, and as
 though it were housing an enum.
 """
+from colored import fgcol, bgcol, colattr
+
 RED = "\033[1;31m"
+GREEN = "\033[1;32m"
+YELLOW = "\033[1;33m"
 BLUE = "\033[1;34m"
+MAGENTA = "\033[1;35m"
 CYAN = "\033[1;36m"
-GREEN = "\033[0;32m"
+LIGHTGRAY = "\033[1;37m"
+
+WHITE = "\033[1;97m"
+
 RESET = "\033[0;0m"
 BOLD = "\033[;1m"
 REVERSE = "\033[;7m"
