2ef97567908a14ad9b56c99710a67d92a671416b
aalok.sathe <aalok.sathe@gmail.com>
2019-01-19 19:24:12 -0500

    start writing cmd loop

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from cmd import Cmd
 from color import *
+from board import Board
 
 print(RED + "colored" + CYAN + "text" + RESET)
+
+class Game(Cmd):
+    board = None
+
+	def __init__(self, *args):
+	       super().__init__()
+
+    def do_new_game(self, arg):
+        '''create a new game'''
